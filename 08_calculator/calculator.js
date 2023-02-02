8ea38c5c75237c66e5dff73c49822dc42be2284d
Dominik <dominikant99@gmail.com>
2023-02-02 15:05:08 +0100

    Complete exercise 08/calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// let sum = 0;
+
+  // for (let i = 0; i< array.length; i++ ) {
+  //   sum += array[i];
+  // }
+
+  // return sum;
+
+  const sum = array.reduce((total, currentValue) =>  total + currentValue, 0);
+  
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  const multiply = array.reduce((total, currentValue) => total * currentValue);
 
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0) return 1;
+
+  let factorialSum = 1;
+
+	for(let i = 1; i <= a; i++) {
+    factorialSum *= i;
+  }
+
+  return factorialSum;
 };
 
 // Do not edit below this line
