8896165a72288978643ce5bb8d2b334d7a2477b2
Dominik <dominikant99@gmail.com>
2023-02-02 20:47:14 +0100

    Complete exercise/10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(numberInSequence) {
+    if(numberInSequence < 0) {
+        return "OOPS";
+    }
 
+    let num1 = 0;
+    let num2 = 1;
+    let num3 = 0;
+
+    for(let i = 1; i < numberInSequence; i++) {
+        num3 = num2 + num1; 
+        num1 = num2;
+        num2 = num3
+    }
+
+    return num3;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
