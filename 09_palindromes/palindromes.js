c5c0d6d77609ef84f59e9066b9d42f2ba4e1f592
Dominik <dominikant99@gmail.com>
2023-02-02 15:34:12 +0100

    Complete exercise 07/palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const processedWord = word.toLowerCase().replace(/[^a-z]/g,"")
 
+    return processedWord.split("").reverse().join("")
 };
 
 // Do not edit below this line
