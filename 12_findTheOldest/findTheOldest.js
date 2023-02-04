f6a032a51e6ded8d42b92af7513d5f836946f8d1
Dominik <dominikant99@gmail.com>
2023-02-04 17:33:10 +0100

    Complete exercise 12/findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        if(!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear()
+        }
 
-};
+        if(!currentPerson.yearOfDeath) {
+            currentPerson.yearOfDeath = new Date().getFullYear()
+        }
+
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+
+        console.log("oldest" + oldest.name)
+        return oldestAge > currentAge ? oldest : currentPerson;
+    });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
