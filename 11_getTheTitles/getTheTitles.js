04d6de3de8c5acc38793cbcafa0e60bb804946d7
Dominik <dominikant99@gmail.com>
2023-02-04 14:57:59 +0100

    Complete exercise 11/getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // booksTitles = [];
+    // books.forEach(book => booksTitles.push(book.title))
 
+    // return booksTitles;
+
+    const titles = books.map(book => book.title);
+
+    return titles;
 };
 
 // Do not edit below this line
